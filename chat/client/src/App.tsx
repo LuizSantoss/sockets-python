8193dc98fd897decb210677696435c133f920ba5
LuizSantoss <lzfelipepro@outlook.com>
2025-10-21 00:23:53 -0300

    pequena grande correção

--- chat/client/src/App.tsx
+++ chat/client/src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { socket } from "./socket"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [ username, setUsername ] = useState<string>();
+  const [ message, setMessage ] = useState <string>();
+  const [ chat, setChat ] = useState<any>([]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("conectado ao servidor");
+    })
+    socket.on("chat_message", (data) => {
+      console.log(data);
+      setChat((prev: any) => [...prev, data]);
+    });
+    
+    return() => {
+      socket.off("chat_message");
+    }
+  }, []);
+
+  const sendMessage = (e: any) => {
+    e.preventDefault();
+    if (!username || !message) return;
+    socket.emit('chat_message', {username, message});
+    setMessage('');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-6 px-48">
+      <h3 className="font-semibold"> chat em tempo real</h3>
+      <div className="flex flex-row items-center space-x-4">
+        <label htmlFor="for-username">nome</label>
+        <input
+          className = "border p-1 rounded w-96" 
+          type = "text" 
+          placeholder="Seu Nome..."
+          value = {username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        </div>
+        <div className="border-h-[300px] overflow-y-auto p-4 mt-2 bg-gray-100">
+          {
+            chat.map((msg: any, i: number) => (
+              <p key={i}> <strong>{msg.username}: {msg.message}</strong></p>
+            ))
+          
+          }
+        </div>
+        <form onSubmit={sendMessage}> 
+          <div className='flex flex-row items-center space-x-4 mt-4'>
+            <input 
+              className="border p-1 rounded w-96"
+              type="text"
+              placeholder="digite sua mensagem..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button
+              className ='border border-sky-800 bg-sky-500 text-white px-4 py-1 rounded hover:bg-sky-400 transition all duration-300 cursor-pointer'
+            >enviar</button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
-
-export default App
